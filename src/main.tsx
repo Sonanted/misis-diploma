126be4ad28a5876e8b88516de48b7848dfee4d4f
Andrey Nikonenko <sonanted@gmail.com>
2026-04-18 16:52:53 +0300

    feat: initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
+import './index.css';
+
 const root = document.getElementById('root');
 
 if (root === null) throw new Error('Failed to find the root element');
